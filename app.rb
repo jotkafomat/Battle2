e440dfc2548b7881ed2621537df9d7cbf43b7b85
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-14 16:45:44 +0100

    used a post redirect loop and enabled a session

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+    enable :sessions
+
     get '/' do
         erb(:index)
     end
 
     post '/names' do
-        @player_1 = params[:Player_1_Name]
-        @player_2 = params[:Player_2_Name]
+        session[:Player_1_Name] = params[:Player_1_Name]
+        session[:Player_2_Name] = params[:Player_2_Name]
+        redirect to('/play')
+    end 
+
+    get '/play' do
+        @player_1 = session[:Player_1_Name]
+        @player_2 = session[:Player_2_Name]
         erb(:play)
     end
+
     run! if app_file == $0
 end
