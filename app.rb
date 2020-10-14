7b95260842c7e8ca9dfa111a290a5a673aac31d4
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-14 16:07:41 +0100

    First capybara feature test attempted, test syntax needs checking

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
     get '/' do
-        "Hello Battle!"
+        erb(:index)
     end
 
+    post '/names' do
+        @player_1 = params[:Player_1_Name]
+        @player_2 = params[:Player_2_Name]
+        erb(:play)
+    end
     run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,7 +1,19 @@
 require './app.rb'
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+#feature 'Testing infrastructure' do
+ # scenario 'Can run app and check page content' do
+ #   visit('/')
+ #   expect(page).to have_content 'Testing infrastructure'
+ # end
+# end
+
+feature 'testing players names are displayed on-screen' do
+  scenario 'Names entered in a form' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure'
+    fill_in 'Player_1_Name:', with: "one"
+    fill_in 'Player_2_Name:', with: "two"
+    click_on("Submit")
+    visit('/names')
+    expect(page).to have_content("Player 1: one Player 2: two")
   end
 end
+
