70ceaa76f585d26609847073a8a84a047bddcc91
jotkafomat <k-j@xl.wp.pl>
2020-10-15 08:59:03 +0100

    add new line in play view

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -9,21 +9,21 @@ require './app.rb'
 feature 'testing players names are displayed on-screen' do
   scenario 'Names entered in a form' do
     visit('/')
-    fill_in :Player_1_Name, with: "one"
-    fill_in :Player_2_Name, with: "two"
+    fill_in :Player_1_Name, with: "Emily"
+    fill_in :Player_2_Name, with: "Krzysztof"
     click_button("Submit")
     # visit('/names')
-    expect(page).to have_content("one vs. two")
+    expect(page).to have_content("Emily vs. Krzysztof")
   end
 end
 
 feature ' Player1 can see Player 2s hit points' do
   scenario 'game play' do
     visit('/')
-    fill_in :Player_1_Name, with: "one"
-    fill_in :Player_2_Name, with: "two"
+    fill_in :Player_1_Name, with: "Emily"
+    fill_in :Player_2_Name, with: "Krzysztof"
     click_button("Submit")
 
-    expect(page).to have_content("Points for player two:")
+    expect(page).to have_content("Krzysztof: 60HP")
   end
 end
